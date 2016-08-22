17090bf86fd3c80122abe9d2ad0b80d43ff413ec
bingoohuang <bingoo.huang@gmail.com>
2016-08-22 19:54:41 +0800

    add flushall

--- conf/poet/mysqldict.lua
+++ conf/poet/mysqldict.lua
@@ -74,23 +74,45 @@ end
 
 local restyLock = require "resty.lock"
 
+-- 清除所有缓存
+-- opt: 相关MySQL信息，表信息等
+--    opt.luaSharedDictName LUA共享字典名
+--    opt.dictLockName  锁名称，在从MySQL刷数据时防止缓存失效风暴
+-- 返回 清除结果
+--      OK 清除成功， 否则为错误信息
+function _M.flushAll(opt)
+    local dict = ngx.shared[opt.luaSharedDictName]
+    local loadedKey = "__loaded_key__" .. opt.luaSharedDictName
+
+    -- 尝试获取锁，获取不到，直接返回错误信息
+    local locker = restyLock:new(opt.dictLockName)
+    local locked = tryLock(locker, loadedKey)
+    if not locked then return "failed to get lock" end
+
+    -- 清除已有缓存数据
+    flushAllCache(dict)
+    unlock(locker)
+    return "OK"
+end
+
 -- lua_shared_dict mysqldict_demo 128m;
 -- lua_shared_dict mysqlDict_lock 100k;
 -- 从缓存中获取数据
--- key: 缓存主键
+
 -- opt: 相关MySQL信息，表信息等
+--    opt.key: 缓存主键
 --    opt.dataSourceName MySQL连接字符串，比如root:my-secret-pw@192.168.99.100:13306/dba
 --    opt.dictTableName  字典表的表名
 --    opt.pkColumnName   字典表的主键字段名
 --    opt.luaSharedDictName LUA共享字典名
 --    opt.dictLockName  锁名称，在从MySQL刷数据时防止缓存失效风暴
--- 返回 val, err
---      缓存key对应的取值，错误信息
-function _M.get(key, opt)
+-- 返回 val
+--      缓存key对应的取值，nil表示缓存值不存在
+function _M.get(opt)
     local dict = ngx.shared[opt.luaSharedDictName]
 
     -- 尝试从缓存中读取，如果读取到，直接返回
-    local val = getFromCache(dict, key)
+    local val = getFromCache(dict, opt.key)
     if val then return val end
 
     -- 查看缓存数据是否已经加载，如果已经加载，则直接返回nil
@@ -104,13 +126,13 @@ function _M.get(key, opt)
     if not locked then return nil end
 
     -- 获取锁后，再次尝试从缓存中读取（因为可能在等待锁时，缓存已经设定好）
-    local val = getFromCache(dict, key)
+    local val = getFromCache(dict, opt.key)
     if val then unlock(lock) return val end
 
     local loaded = getFromCache(dict, loadedKey)
     if loaded == "yes" then return nil end
 
-    -- 清楚已有缓存数据
+    -- 清除已有缓存数据
     flushAllCache(dict)
     -- 从数据库字典表中加载数据到缓存
     local db, err = connectMySQL(opt.dataSourceName)
@@ -128,7 +150,7 @@ function _M.get(key, opt)
         error("bad result: " .. err)
     end
 
-    return getFromCache(dict, key)
+    return getFromCache(dict, opt.key)
 end
 
 
