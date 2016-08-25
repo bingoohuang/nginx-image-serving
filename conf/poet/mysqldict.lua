c6655c421e2782a3aba9cd60edafb23b40625cad
bingoohuang <bingoo.huang@gmail.com>
2016-08-25 08:52:13 +0800

    add prefix for dict sharing among multiple caches

--- conf/poet/mysqldict.lua
+++ conf/poet/mysqldict.lua
@@ -1,5 +1,8 @@
 -- 本模块提供字典读取缓存功能
 -- 第一次调用时，从MySQL中读取字典数据表所有需要缓存的数据（数量较少更新不频繁的表）
+--- 为什么要一次性从数据库中加载所有的缓存数据呢？
+--                    1) 效率高，只需要访问一次数据库
+--                    2) 避免纯消耗的空查询（每次查询KEY都没有结果，每次都要访问数据库）
 --------------将字典数据做成nginx的dict缓存
 --------------根据key从缓存中读取数据
 -- 后续调用时，直接从缓存中读取数据
@@ -9,13 +12,17 @@ local _M = {
     _VERSION = '0.1'
 }
 
+local shared = ngx.shared
+local cjson = require "cjson"
+local restyLock = require "resty.lock"
+local mysql = require "resty.mysql"
+
 local function error(msg)
     ngx.status = 500 ngx.say(msg)
     ngx.log(ngx.ERR, msg) ngx.exit(500)
 end
 
 local function connectMySQL(dataSourceName)
-    local mysql = require "resty.mysql"
     local db, err = mysql:new()
     if not db then return nil, "failed to instantiate mysql" .. err end
     db:set_timeout(1000) -- 1 second
@@ -39,18 +46,6 @@ local function closeDb(db)
     db:close()
 end
 
-local function tryLock(locker, key)
-    local elapsed, err = locker:lock(key)
-    return elapsed
-end
-
-local function unlock(locker)
-    local unlockok, err = locker:unlock()
-    if not unlockok then error("failed to unlock: " .. err) end
-end
-
-local cjson = require "cjson"
-
 local function getFromCache(dict, key)
    local value = dict:get(key)
    if not value then return nil end
@@ -58,38 +53,47 @@ local function getFromCache(dict, key)
    return cjson.decode(value)
 end
 
-local function setToCache(dict, rows, pkColumnName)
+local function setToCache(dict, prefix, rows, pkColumnName)
    for k,v in pairs(rows) do
-       local key = v[pkColumnName]
+       local key = prefix .. v[pkColumnName]
        local val = cjson.encode(v)
        local succ, err, forcible = dict:set(key, val)
    end
 end
 
-local function flushAllCache(dict)
-    dict:flush_all()
-end
+local function startsWith(str, substr)
+    if str == nil or substr == nil then
+        return nil, "the string or the sub-stirng parameter is nil"
+    end
 
-local restyLock = require "resty.lock"
+    return string.find(str, substr) == 1
+end
 
 -- 清除所有缓存
 -- opt: 相关MySQL信息，表信息等
 --    opt.luaSharedDictName LUA共享字典名
 --    opt.dictLockName  锁名称，在从MySQL刷数据时防止缓存失效风暴
+--    opt.prefix  可选。前缀名，当多个不同缓存使用同一个luaSharedDictName和dictLockName时，使用前缀加以区分
+
 -- 返回 清除结果
---      OK 清除成功， 否则为错误信息
+--    OK 清除成功， 否则为错误信息
 function _M.flushAll(opt)
-    local dict = ngx.shared[opt.luaSharedDictName]
-    local loadedKey = "__loaded_key__" .. opt.luaSharedDictName
+    local dict = shared[opt.luaSharedDictName]
+    local prefix = (opt.prefix or "__default_prefix") .. "."
+    local loadedKey = prefix .. "__loaded_key__" .. opt.luaSharedDictName
 
     -- 尝试获取锁，获取不到，直接返回错误信息
     local locker = restyLock:new(opt.dictLockName)
-    local locked = tryLock(locker, loadedKey)
+    local locked, err = locker:lock(loadedKey)
     if not locked then return "failed to get lock" end
 
-    -- 清除已有缓存数据
-    flushAllCache(dict)
-    unlock(locker)
+    -- dict:flush_all() -- 清除已有缓存数据
+    local keys = dict:get_keys(0)
+    for index,dictKey in pairs(keys) do
+        if startsWith(dictKey, prefix) then dict:delete(dictKey) end
+    end
+
+    locker:unlock()
     return "OK"
 end
 
@@ -103,53 +107,50 @@ end
 --    opt.queryAllSql 查询用的SQL语句，用于一次性从数据库中查询所有需要缓存的数据，一次性缓存后就不再访问数据库了
 --    opt.pkColumnName   字典表的主键字段名
 --    opt.luaSharedDictName LUA共享字典名
---    opt.dictLockName  锁名称，在从MySQL刷数据时防止缓存失效风暴
+--    opt.dictLockName  锁名称，在从MySQL刷数据时防止缓存失效风暴，可以多个luaSharedDictName共用
+--    opt.prefix  可选。前缀名，当多个不同缓存使用同一个luaSharedDictName和dictLockName时，使用前缀加以区分
+
 -- 返回 val
---      缓存key对应的取值，nil表示缓存值不存在
+--    缓存key对应的取值，nil表示缓存值不存在
 function _M.get(opt)
-    local dict = ngx.shared[opt.luaSharedDictName]
+    local dict = shared[opt.luaSharedDictName]
+    local prefix = (opt.prefix or "__default_prefix") .. "."
+    local cacheKey = prefix .. opt.key
+    local loadedKey = prefix .. "__loaded_key__" .. opt.luaSharedDictName
 
     -- 尝试从缓存中读取，如果读取到，直接返回
-    local val = getFromCache(dict, opt.key)
+    local val = getFromCache(dict, cacheKey)
     if val then return val end
-
     -- 查看缓存数据是否已经加载，如果已经加载，则直接返回nil
-    local loadedKey = "__loaded_key__" .. opt.luaSharedDictName
     local loaded = getFromCache(dict, loadedKey)
     if loaded == "yes" then return nil end
 
     -- 尝试获取锁，获取不到，直接返回nil
     local locker = restyLock:new(opt.dictLockName)
-    local locked = tryLock(locker, loadedKey)
+    local locked, err = locker:lock(loadedKey)
     if not locked then return nil end
 
     -- 获取锁后，再次尝试从缓存中读取（因为可能在等待锁时，缓存已经设定好）
-    local val = getFromCache(dict, opt.key)
-    if val then unlock(lock) return val end
-
+    local val = getFromCache(dict, cacheKey)
+    if val then locker:unlock() return val end
+    -- 查看缓存数据是否已经加载，如果已经加载，则直接返回nil
     local loaded = getFromCache(dict, loadedKey)
-    if loaded == "yes" then return nil end
+    if loaded == "yes" then locker:unlock() return nil end
 
-    -- 清除已有缓存数据
-    flushAllCache(dict)
+    dict:flush_all() -- 清除已有缓存数据
+
     -- 从数据库字典表中加载数据到缓存
     local db, err = connectMySQL(opt.dataSourceName)
-    if not db then unlock(locker) error(err) end
-
+    if not db then locker:unlock() error(err) end
     local rows, err, errcode, sqlstate = db:query(opt.queryAllSql)
     if rows then
-        setToCache(dict, rows, opt.pkColumnName)
+        setToCache(dict, prefix, rows, opt.pkColumnName)
         dict:set(loadedKey, "yes")
     end
+    locker:unlock() closeDb(db)
+    if not rows then error("bad result: " .. err) end
 
-    unlock(locker) closeDb(db)
-
-    if not rows then
-        error("bad result: " .. err)
-    end
-
-    return getFromCache(dict, opt.key)
+    return getFromCache(dict, cacheKey)
 end
-
 
 return _M
